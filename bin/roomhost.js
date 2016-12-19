de68b167c1b62078d76e7969695344b9df7bf6d1
antonwass <anton_wass@hotmail.com>
2016-12-19 21:28:02 +0100

    added tcp server

--- bin/roomhost.js
+++ bin/roomhost.js
@@ -1,6 +1,5 @@
 var net = require('net');
-
-var hosts = [];
+var partyroom = require('../bin/partyroom');
 
 var server = net.createServer(function(socket){
 
@@ -12,20 +11,32 @@ var server = net.createServer(function(socket){
 
 server.on('connection', function(socket){
     socket.setEncoding('utf8');
+
     socket.on('data', function(data){
         console.log('received data: ' + data);
+
+        var msg = JSON.parse(message);
+
+        switch(msg.type){
+            case 'authenticate':
+                console.log('Authenticating client with token: ' + msg.access_token);
+
+                var room = partyroom.createRoom();
+
+                var response = {
+                    type:'room',
+                    key:room.key
+                };
+
+                socket.send(JSON.stringify(response));
+
+                break;
+        }
     });
 });
 
 server.listen(12345, function(){
     console.log('server opened on ', server.address());
 });
-
-
-function handleMessage(message){
-    //parse to json
-    //switch
-}
-
 
 
---
+++ bin/spotifyapi.js
@@ -0,0 +1,7 @@
+var SpotifyWebApi = require('spotify-web-api-node');
+
+var spotifyApi = new SpotifyWebApi({
+    clientId:'',
+    clientSecret:'',
+    redirectUri:''
+});
